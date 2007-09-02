88ef45d801384fae2534146249a133a95af81055
ian <none@none>
2007-09-02 19:53:05 +0000

    add comparable
    --HG--
    branch : Devel
    extra : convert_revision : svn%3Aea8264ff-aabd-0310-85a2-8f2bf4176de1/Devel%40299

--- lib/barcode/barcode.rb
+++ lib/barcode/barcode.rb
@@ -1,31 +1,32 @@
 module Barcode
 
 class Barcode
-  attr_reader :code
+    include Comparable
+    attr_reader :code
 
-  def initialize(code)
-      required = required_length()
-      if required && code.length != required then
-          raise "#{type()} must be #{required} characters long"
-      end
-      @code = code
-  end
+    def initialize(code)
+        required = required_length()
+        if required && code.length != required then
+            raise "#{type()} must be #{required} characters long"
+        end
+        @code = code
+    end
 
-  def <=>(other)
-      @code <=> other.code
-  end
+    def <=>(other)
+        @code <=> other.code
+    end
 
-  def to_s
-      @code
-  end
+    def to_s
+        @code
+    end
 
-  def required_length
-      return nil
-  end
+    def required_length
+        return nil
+    end
 
-  def is_isbn
-      return false
-  end
+    def is_isbn
+        return false
+    end
 
 end
 
