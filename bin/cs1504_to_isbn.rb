560a1674173ede254f98eeb4135e0d4d1e1ccfc0
ian <none@none>
2007-09-02 03:20:04 +0000

    change to cs1504factory
    --HG--
    branch : Devel
    rename : lib/barcode/cs1504.rb => lib/barcode/cs1504factory.rb
    extra : convert_revision : svn%3Aea8264ff-aabd-0310-85a2-8f2bf4176de1/Devel%40287

--- bin/cs1504_to_isbn.rb
+++ bin/cs1504_to_isbn.rb
@@ -3,10 +3,9 @@
 require "barcode/cs1504"
 
 while line = gets
-    type, code, timestamp = line.split(/\|/)
-    cc = Barcode::CS1504Code.new(code, type, timestamp)
-    begin
-        bc = cc.to_barcode()
+    begin 
+        type, code, timestamp = line.split(/\|/)
+        bc = Barcode::CS1504Factory.new(type, code)
         puts bc.to_isbn.to_s
     rescue
         $stderr.puts "#{type}:#{code} failed: #{$!}"
--- lib/barcode/cs1504factory.rb
+++ lib/barcode/cs1504factory.rb
@@ -6,22 +6,15 @@ require "barcode/ean13"
 require "barcode/upca"
 require "barcode/upcaplus5"
 
-class CS1504Code < Barcode
-    attr_reader :type, :timestamp
+class CS1504Factory
 
-    def initialize(code, type, timestamp)
-        super(code)
-        @type = type
-        @timestamp = timestamp
-    end
-
-    def to_barcode
-        barcode_class = CS1504Code.type_to_class(@type)
+    def CS15046Factory.new_barcode(type, code)
+        barcode_class = type_to_class(@type)
         raise "Unknown type #{@type}" unless barcode_class
         return barcode_class.new(@code)
     end
 
-    def CS1504Code.type_to_class(type)
+    def CS1504Factory.type_to_class(type)
         case type
         when "EAN-13"
             return EAN13
