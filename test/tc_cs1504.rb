d44f354cec515aebe41fb3456674a4ea37bdc972
ian <none@none>
2007-09-02 05:26:02 +0000

    consolidate cs1504 tests
    --HG--
    branch : Devel
    extra : convert_revision : svn%3Aea8264ff-aabd-0310-85a2-8f2bf4176de1/Devel%40295

--- test/tc_cs1504.rb
+++ test/tc_cs1504.rb
@@ -6,6 +6,7 @@ require "test/unit"
 require "cs1504"
 
 class TC_Commands < Test::Unit::TestCase
+
   def test_make_command
     cs1504 = CS1504.new("/dev/ttyS0")
     assert_equal(cs1504.make_command(1), "\x01\x02\x00\x9F\xDE")
@@ -15,6 +16,11 @@ class TC_Commands < Test::Unit::TestCase
     assert_equal(cs1504.make_command(5), "\x05\x02\x00\x5E\x9F")
     assert_equal(cs1504.make_command(7), "\x07\x02\x00\x9E\x3E")
     assert_equal(cs1504.make_command(10), "\x0A\x02\x00\x5D\xAF")
+  end
+
+  def test_interrogate
+    cs1504 = CS1504.new("/dev/ttyS0")
+    $stderr.print(cs1504.interrogate())
   end
 
 end
