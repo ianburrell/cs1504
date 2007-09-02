1b72ca9ae1fb32ec9541003b83b0a1bec06a2ec0
ian <none@none>
2007-09-02 05:56:05 +0000

    add clear
    --HG--
    branch : Devel
    extra : convert_revision : svn%3Aea8264ff-aabd-0310-85a2-8f2bf4176de1/Devel%40297

--- bin/dump.sh
+++ bin/dump.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-date=$(date +%Y%m%D_%H%M%S)
-ruby -Ilib bin/cs1504_dump.rb > dump_$date.txt
-ruby -Ilib bin/cs1504_to_isbn.rn < dump_$date.txt > isbn_$date.txt
+#!/bin/bash -x
+date=$(date +%Y%m%d_%H%M%S)
+ruby -Ilib bin/cs1504_dump.rb --clear > dump_$date.txt
+ruby -Ilib bin/cs1504_to_isbn.rb < dump_$date.txt > isbn_$date.txt
